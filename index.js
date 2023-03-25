8b5bb23cdc3abcff0e4c3b36d5c2d5a504ac1480
Andresforni <123376922+Andresforni@users.noreply.github.com>
2023-03-25 15:00:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('./telegram-bot.js');
+
+const token = '5693480247:AAGiD0rd5a3uIiuHrIcclac8Zjna0e--Edo';
+
+const exampleCallback = (message) => {
+	if (message.action == '/start') {
+		return 'Hello World!';
+	}
+	if (message.action == '/hello') {
+		if (message.text.split(' ').length == 2) {
+			return 'Hello, ' + message.text.split(' ')[1];
+		} else {
+			return 'Hello, Noname';
+		}
+	}
+	if (message.action == '/me') {
+		const firstName = message.message.from.first_name;
+		const lastName = typeof message.message.from.last_name !== undefined ? message.message.from.last_name : '';
+		const username = typeof message.message.from.username !== undefined ? message.message.from.username : '';
+
+		return 'You\'re ' + firstName + (lastName !== '' ? ' ' + lastName : '') + (username !== '' ? ' (@' + username + ')' : '');
+	}
+	if (message.action == '/weather') {
+		return '+25°C';
+	}
+};
+
+const exampleBot = new TelegramBot(token, exampleCallback);
+console.log("iniciando...");
+exampleBot.start();
